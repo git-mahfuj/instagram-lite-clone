213caee479d735d33a0a26a3faaede239e186eb1
git-mahfuj <mahfujurrheeck@gmail.com>
2026-04-22 01:38:51 +0600

    feat : implemented profile ui with realtime-context

--- src/components/MobileView/MobileBottomMenu.tsx
+++ src/components/MobileView/MobileBottomMenu.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React from "react";
 import { House, Search, Play, Send } from "lucide-react";
@@ -6,9 +6,9 @@ import Image from "next/image";
 import { useUser } from "@/contexts/UserContext";
 
 const MobileBottomMenu = () => {
-  const {user} = useUser()
+  const { user } = useUser();
   return (
-    <div className=" bottom-0 fixed border border-t w-full py-4 md:hidden ">
+    <div className="bg-white dark:bg-black bottom-0 fixed border border-t w-full py-4 md:hidden ">
       <div className="flex justify-around items-center gap-11 px-5">
         <Link href={"/"}>
           <House />
@@ -24,11 +24,11 @@ const MobileBottomMenu = () => {
         </Link>
         <Link href={"/profile"}>
           <Image
-          src={user?.profileImage?.url as string}
-                  alt=""
-                  height={10}
-                  width={30}
-                  className="aspect-square object-cover rounded-full bg-black dark:bg-white"
+            src={user?.profileImage?.url as string}
+            alt=""
+            height={10}
+            width={30}
+            className="aspect-square object-cover rounded-full bg-black dark:bg-white"
           ></Image>
         </Link>
       </div>
